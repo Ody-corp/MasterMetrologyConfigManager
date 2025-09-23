a7859648eccbbd835b1c8add31f39f7d25b1219e
Tomáš Tejiščák <tomas.tejiscak@gmail.com>
2025-09-23 12:18:14 +0200

    changed InputModelData for databinding

--- MasterMetrology/Models/Data/InputModelData.cs
+++ MasterMetrology/Models/Data/InputModelData.cs
@@ -1,14 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MasterMetrology.Models.Data
 {
-    internal class InputsDefModelData
+    internal class InputsDefModelData : INotifyPropertyChanged
     {
-        public string ID { get; set; }
-        public string Name { get; set; }
+        private string id;
+        private string name;
+
+        public string ID
+        {
+            get => id;
+            set
+            {
+                if (id != value)
+                {
+                    id = value;
+                    OnPropertyChanged(nameof(ID));
+                }
+            }
+        }
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
