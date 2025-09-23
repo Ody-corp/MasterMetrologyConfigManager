3fb1e5e1a6274ae17b876fac5afb1968715b4e86
Tomáš Tejiščák <tomas.tejiscak@gmail.com>
2025-09-23 12:21:09 +0200

    changed TransitionModelData for databinding

--- MasterMetrology/Models/Data/TransitionModelData.cs
+++ MasterMetrology/Models/Data/TransitionModelData.cs
@@ -1,14 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MasterMetrology.Models.Data
 {
-    internal class TransitionModelData
+    internal class TransitionModelData : INotifyPropertyChanged
     {
-        public string Input {  get; set; }
-        public string NextState { get; set; }
+        private string input;
+        private string nextStage;
+
+        public string Input
+        {
+            get => input;
+            set
+            {
+                if (input != value)
+                {
+                    input = value;
+                    OnPropertyChanged(nameof(Input));
+                }
+            }
+        }
+        public string NextStage
+        {
+            get => nextStage;
+            set
+            {
+                if (nextStage != value)
+                {
+                    nextStage = value;
+                    OnPropertyChanged(nameof(NextStage));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
